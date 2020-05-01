b2c9a69a2efdfc093458557fb8845eaf07ce2ec7
Rafael Gil <rafaelgil83@gmail.com>
2020-05-01 15:30:04 -0400

    - working plex class with healthcheck

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 const axios = require("axios");
 const cheerio = require('cheerio');
 require('dotenv').config();
 
 const MejorEnVo = require("./lib/MejorEnVo");
 const Transmission = require("./lib/GetTorrent");
+const Plex = require("./lib/Plex");
 
 (async () => {
 
+    // plex.search("Anunnaki");
+    // client.query("/library/metadata/304222").then(function (directories) {
+    //     console.log(directories);
+    //     // directories would be an array of sections whose type are "movie"
+    // }, function (err) {
+    //     console.error("Could not connect to server", err);
+    // });
+
+    // client.query("/").then(function (result) {
+    //     console.log(result);
+    // }, function (err) {
+    //     console.error("Could not connect to server", err);
+    // });
+
+
+    const plex = new Plex();
     const transmission = new Transmission();
 
-    function init() {
-        console.log("Initiating transmission server healthcheck")
-        transmission.getTransmissionStats(function (err, result) {
-            if (!err) {
-                console.log(result);
-            } else {
-                console.log("Could not contact transmission server", err);
-            }
-        });
+    if (await plex.healthCheck()) {
+
+
+        // transmission.getTransmissionStats(function (err, result) {
+        //     if (!err) {
+        //         console.log(result);
+        //     } else {
+        //         console.log("Could not contact transmission server", err);
+        //     }
+        // });
+
+
+        console.log("Script continues");
     }
 
-    // init();
 
+    // const search = await plex.search("superman");
+    // console.log("Search result", plexHealthCheck);
 
-    const mejorEnVo = new MejorEnVo(axios, cheerio);
-    mejorEnVo.init().then((response) => {
-        console.log(response);
-    });
-    console.log(await mejorEnVo.init());
+    // const mejorEnVo = new MejorEnVo(axios, cheerio);
+    // mejorEnVo.init().then((response) => {
+    //     console.log(response);
+    // });
+    // console.log(await mejorEnVo.init());
 
     // const fetchData = async (url) => {
     //     const result = await axios.get(url);
